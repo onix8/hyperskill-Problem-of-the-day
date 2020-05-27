32528dcfa4b55d3520974076bd9c87fac8822766
ONIX <zachem-nado@mail.ru>
2020-05-27 17:53:49 +0300

    Stage 5/6: On a coffee loop (Solution)

--- Coffee Machine/task/src/machine/CoffeeMachine.java
+++ Coffee Machine/task/src/machine/CoffeeMachine.java
@@ -28,27 +28,25 @@ public class CoffeeMachine {
 
     public static void main(String[] args) {
 
-        state();
-
-        System.out.println("\nWrite action (buy, fill, take):");
-        String user = scanner.nextLine();
-        mode(user);
-
-        System.out.println();
-        state();
+        String action;
+        do {
+            action = scanner.nextLine();
+            mode(action);
+        } while (!"exit".equals(action));
     }
 
-    private static void state() {
-        System.out.println("The coffee machine has:");
+    private static void remaining() {
+        System.out.println("\nThe coffee machine has:");
         System.out.printf("%d of water\n", water);
         System.out.printf("%d of milk\n", milk);
         System.out.printf("%d of coffee beans\n", coffeeBeans);
         System.out.printf("%d of disposable cups\n", cups);
-        System.out.printf("%d of money\n", money);
+        System.out.printf("%d of money\n\n", money);
     }
 
-    private static void mode(String user) {
-        switch (user) {
+    private static void mode(String action) {
+        System.out.println("Write action (buy, fill, take, remaining, exit):");
+        switch (action) {
             case "buy":
                 buy();
                 break;
@@ -58,25 +56,105 @@ public class CoffeeMachine {
             case "take":
                 take();
                 break;
+            case "remaining":
+                remaining();
+                break;
+            case "exit":
+                break;
         }
     }
 
     private static void buy() {
-        System.out.println("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino:");
-        int type = scanner.nextInt();
+        System.out.println("\nWhat do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino, back - to main menu:");
+        String type = scanner.nextLine();
         switch (type) {
-            case 1:
-                money += ESPRESSO_MONEY;
-                createEspresso();
+            case "1":
+                if (enoughResources(type)) {
+                    System.out.println("I have enough resources, making you a coffee!\n");
+                    money += ESPRESSO_MONEY;
+                    createEspresso();
+                } else {
+                    System.out.printf("Sorry, not enough %s!\n\n", whatIsMissing(type));
+                }
                 break;
-            case 2:
-                money += LATTE_MONEY;
-                createLatte();
+            case "2":
+                if (enoughResources(type)) {
+                    System.out.println("I have enough resources, making you a coffee!\n");
+                    money += LATTE_MONEY;
+                    createLatte();
+                } else {
+                    System.out.printf("Sorry, not enough %s!\n\n", whatIsMissing(type));
+                }
                 break;
-            case 3:
-                money += CAPPUCCINO_MONEY;
-                createCappuccino();
+            case "3":
+                if (enoughResources(type)) {
+                    System.out.println("I have enough resources, making you a coffee!\n");
+                    money += CAPPUCCINO_MONEY;
+                    createCappuccino();
+                } else {
+                    System.out.printf("Sorry, not enough %s!\n\n", whatIsMissing(type));
+                }
                 break;
+            case "back":
+                break;
+            default:
+                buy();
+        }
+    }
+
+    private static boolean enoughResources(String type) {
+        switch (type) {
+            case "1":
+                return water - ESPRESSO_ML_WATER_ON_ONE_CUP >= 0
+                        && coffeeBeans - ESPRESSO_G_COFFEE_BEANS_ON_ONE_CUP >= 0
+                        && cups - 1 >= 0;
+            case "2":
+                return water - LATTE_ML_WATER_ON_ONE_CUP >= 0
+                        && milk - LATTE_ML_MILK_ON_ONE_CUP >= 0
+                        && coffeeBeans - LATTE_G_COFFEE_BEANS_ON_ONE_CUP >= 0
+                        && cups - 1 >= 0;
+            case "3":
+                return water - CAPPUCCINO_ML_WATER_ON_ONE_CUP  >= 0
+                        && milk - CAPPUCCINO_ML_MILK_ON_ONE_CUP >= 0
+                        && coffeeBeans - CAPPUCCINO_G_COFFEE_BEANS_ON_ONE_CUP >= 0
+                        && cups - 1 >= 0;
+            default:
+                throw new IllegalStateException("Unexpected value: " + type);
+        }
+    }
+
+    private static String whatIsMissing(String type) {
+        switch (type) {
+            case "1":
+                if (water - ESPRESSO_ML_WATER_ON_ONE_CUP < 0) {
+                    return "water";
+                } else if (coffeeBeans - ESPRESSO_G_COFFEE_BEANS_ON_ONE_CUP < 0) {
+                    return "coffeeBeans";
+                } else {
+                    return "cups";
+                }
+            case "2":
+                if (water - LATTE_ML_WATER_ON_ONE_CUP < 0) {
+                    return "water";
+                } else if (milk - LATTE_ML_MILK_ON_ONE_CUP < 0) {
+                    return "milk";
+                } else if (coffeeBeans - LATTE_G_COFFEE_BEANS_ON_ONE_CUP < 0) {
+                    return "coffeeBeans";
+                } else {
+                    return "cups";
+                }
+            case "3":
+                if (water - CAPPUCCINO_ML_WATER_ON_ONE_CUP < 0) {
+                    return "water";
+                } else if (milk - CAPPUCCINO_ML_MILK_ON_ONE_CUP < 0) {
+                    return "milk";
+                } else if (coffeeBeans - CAPPUCCINO_G_COFFEE_BEANS_ON_ONE_CUP < 0) {
+                    return "coffeeBeans";
+                } else {
+                    return "cups";
+                }
+            default:
+                throw new IllegalStateException("Unexpected value: " + type);
         }
     }
 
@@ -101,7 +179,7 @@ public class CoffeeMachine {
     }
 
     private static void fill() {
-        System.out.println("Write how many ml of water do you want to add:");
+        System.out.println("\nWrite how many ml of water do you want to add:");
         water += scanner.nextInt();
         System.out.println("Write how many ml of milk do you want to add:");
         milk += scanner.nextInt();
@@ -112,7 +190,7 @@ public class CoffeeMachine {
     }
 
     private static void take() {
-        System.out.printf("I gave you $%d\n", money);
+        System.out.printf("\nI gave you $%d\n\n", money);
         money = 0;
     }
 }
