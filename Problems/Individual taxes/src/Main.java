e0cfccc55f8d0238825403d9a451791dcbf2cd15
ONIX <zachem-nado@mail.ru>
2020-05-29 16:37:08 +0300

    Solution

--- Problems/Individual taxes/src/Main.java
+++ Problems/Individual taxes/src/Main.java
@@ -2,6 +2,22 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        // write your code here
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[] ints = new int[n];
+        double[] ints1 = new double[n];
+        for (int i = 0; i < n; i++) {
+            ints[i] = scanner.nextInt();
+        }
+        int index = 1;
+        double max = 0;
+        for (int i = 0; i < n; i++) {
+            ints1[i] = (ints[i] / 100d) * scanner.nextInt();
+            if (ints1[i] > max) {
+                index = i + 1;
+                max = ints1[i];
+            }
+        }
+        System.out.println(index);
     }
 }
