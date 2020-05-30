94064b3ca04aad25e611581376fa528d72dfdc99
ONIX <zachem-nado@mail.ru>
2020-05-30 14:45:56 +0300

    Solution

--- Problems/The largest element of a sequence/src/Main.java
+++ Problems/The largest element of a sequence/src/Main.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+
 class Main {
     public static void main(String[] args) {
-        // put your code here
+        Scanner scanner = new Scanner(System.in);
+        int max = Integer.MIN_VALUE;
+        int next;
+        while (scanner.hasNextInt()) {
+            next = scanner.nextInt();
+            if (next == 0) {
+                break;
+            }
+            if (next > max) {
+                max = next;
+            }
+        }
+        System.out.println(max);
     }
 }
